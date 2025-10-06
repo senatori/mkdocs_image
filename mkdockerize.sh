c28fb0866aabbb03c9f8777d6cb11d1f5c311109
Steven Senatori <senatori@users.noreply.github.com>
2025-10-05 23:07:33 -0700

    Serve logic

--- mkdockerize.sh
+++ mkdockerize.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
+# To Produce
+
 docker run -v $(pwd)/mkdoc_local:/input_dir mkdocs:0.0.1 produce > docs.tar.gz
+
+# To serve
+cat docs.tar.gz | docker run -i -p 8000:8000 mkdocs:0.0.1 serve
--- run.sh
+++ run.sh
@@ -5,6 +5,12 @@ echo "First argument: $1" &>2
 
 if [ "$1" = "serve" ]; then
     echo "Run serve mode" 
+    mkdir serve_dir
+     tar -xzf - -C serve_dir
+    echo "Starting server"
+    cd serve_dir
+    mkdocs serve --dev-addr 0.0.0.0:8000
+    mkdocs serve -f serve_dir/mkdocs.yml -a
 elif [ "$1" = "produce" ]; then
 
     mkdocs build -f /input_dir/mkdocs.yml
